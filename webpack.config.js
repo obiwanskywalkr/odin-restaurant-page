b91620432fb7fe2cdbbe49f66409924b6071f242
obiwanskywalkr <gp47spd13@gmail.com>
2022-05-19 13:11:31 -0500

    Initial commit for config files

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        client: {
+            logging: 'error',
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant Page'
+        }),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
