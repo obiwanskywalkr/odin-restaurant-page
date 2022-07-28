ff0ee3ff8154568703060225188e534a514c61ee
obiwanskywalkr <gp47spd13@gmail.com>
2022-07-28 17:19:28 -0500

    Commit for build in prod mode with new dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         index: './src/index.js',
     },
@@ -33,6 +33,17 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(pdf)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        },
+                    },
+                ],
             },
             {
                 test: /\.(png)$/i,
